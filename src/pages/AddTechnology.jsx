f6f975b1707b953db6a622c47116edd07558bc34
Dark-Counter <workprogrammail@gmail.com>
2025-11-28 20:50:18 +0300

    fix 24 practic

--- src/pages/AddTechnology.jsx
+++ src/pages/AddTechnology.jsx
@@ -45,7 +45,7 @@ function AddTechnology() {
     return Object.keys(newErrors).length === 0
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     
     if (!validate()) {
